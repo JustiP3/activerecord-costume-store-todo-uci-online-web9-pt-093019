4dbc289450a8a340379dc2bd1e0358bbb8cc284e
Justin Phillips <justinphil02@yahoo.com>
2019-12-21 22:06:25 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       column.string :location
       column.string :theme 
       column.float :price 
-      column.boolean :friendly
+      column.boolean :family_friendly
       column.datetime :opening_date
       column.datetime :closing_date
       column.text :long_description 
